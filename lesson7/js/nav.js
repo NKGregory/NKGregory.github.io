e5c8af690123cd4e419c859cd01a900ea61dad9b
NKGregory <b01194921@byui.edu>
2021-02-17 09:35:39 -0700

    just html

--- lesson7/js/nav.js
+++
@@ -1,6 +0,0 @@
-const hamopen = document.querySelector('.ham');
-const hamclose = document.querySelector('.hamclose');
-const mainnav = document.querySelector('.navigation')
-
-hamopen.addEventListener('click', () => {mainnav.classList.toggle('responsive')}, false);
-hamclose.addEventListener('click', () => {mainnav.classList.toggle('responsive')}, false);
