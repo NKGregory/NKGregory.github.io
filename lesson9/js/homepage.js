91544f1f8876a3a4f8ed833b3dc092769fa12feb
NKGregory <b01194921@byui.edu>
2021-03-06 11:06:31 -0700

    lunch push

--- lesson9/js/homepage.js
+++ lesson9/js/homepage.js
@@ -27,24 +27,27 @@ fetch(requestURL)
         if(towns[i].name === "Preston" || towns[i].name === "Fish Haven" || towns[i].name === "Soda Springs") {
 
             let card = document.createElement('section');
-            let name = document.createElement('h2');
+
+            let name = document.createElement('h3');
             let motto = document.createElement('p');
             let yearFounded = document.createElement('p');
             let currentPopulation = document.createElement('p');
             let averageRainfall = document.createElement('p');
-
+            let image = document.createElement('img');
 
             name.textContent = towns[i].name;
             motto.textContent = towns[i].motto;
             yearFounded.textContent = 'Year Founded: '+ towns[i].yearFounded;
             currentPopulation.textContent = 'Current Population: '+ towns[i].currentPopulation;
-            averageRainfall.textContent = 'Average Rainfall: '+ towns[i].averageRainfall;
+            averageRainfall.textContent = 'Average Annual Rainfall: '+ towns[i].averageRainfall + ' inches';
+            image.setAttribute('src', "images/index/" + towns[i].photo);
 
             card.appendChild(name);
             card.appendChild(motto);
             card.appendChild(yearFounded);
             card.appendChild(currentPopulation);
             card.appendChild(averageRainfall);
+            card.appendChild(image);
 
             document.querySelector('div.cards').appendChild(card);
         }
