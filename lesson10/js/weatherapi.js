260f3e6dd90fe7bab74328d51490f4cde6cbfee0
NKGregory <b01194921@byui.edu>
2021-03-09 00:36:11 -0700

    midnight

--- lesson10/js/weatherapi.js
+++ lesson10/js/weatherapi.js
@@ -3,6 +3,7 @@ fetch(apiURL)
   .then((response) => response.json())
   .then((jsObject) => {
     console.log(jsObject);
+    document.getElementById('currently').textContent = jsObject.weather[0].description;
     document.getElementById('current-temp').textContent = jsObject.main.temp;
     document.getElementById('humidity').textContent = jsObject.main.humidity;
     document.getElementById('wind-speed').textContent = jsObject.wind.speed;
