395539b9f035fffec11e5eed8d99a78806e84e9b
NKGregory <b01194921@byui.edu>
2021-03-10 21:13:38 -0700

    until I can look at lab

--- lesson10/js/weatherapi.js
+++ lesson10/js/weatherapi.js
@@ -14,5 +14,27 @@ fetch(foreapiURL)
   .then((response) => response.json())
   .then((jsObject) => {
     console.log(jsObject);
-   
+    const substring = '18:00:00'
+    for (let i = 0; i < jsObject.length; i++ ) {
+      if(jsObject.list[i].dt_txt.includes("18:00")) {
+        let card = document.createElement('section');
+        let day = document.createElement('p')
+        let picture = document.createElement('img')
+        let temp = document.createElement('p')
+
+        card.setAttribute('class', 'forcastday');
+        day.textContent = jsObject.list[i].main.day;
+        const imagesrc = 'https://openweathermap.org/img/w/' + jsObject.list[i].weather[0].icon + '.png';  // note the concatenation
+        const desc = jsObject.list[i].weather[0].description;  // note how we reference the weather array
+        picture.setAttribute('src', imagesrc);  // focus on the setAttribute() method
+        picture.setAttribute('alt', desc);
+        temp.innerHTML = Math.round(jsObject.list[i].main.temp)+"&deg;"+" F";
+
+        card.appendChild(day);
+        card.appendChild(picture);
+        card.appendChild(temp);
+
+        document.querySelector('div.forcasttable').appendChild(card);
+      }
+    }
   });
