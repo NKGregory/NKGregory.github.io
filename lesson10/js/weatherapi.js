a19491a9d54aae46bc32335d7d3f8d524b0c77c3
NKGregory <b01194921@byui.edu>
2021-03-11 21:07:36 -0700

    fixing api reference

--- lesson10/js/weatherapi.js
+++ lesson10/js/weatherapi.js
@@ -4,7 +4,7 @@ fetch(summaryapiURL)
   .then((jsObject) => {
     //console.log(jsObject);
     document.getElementById('currently').textContent = jsObject.weather[0].description;
-    document.getElementById('current-temp').textContent = jsObject.main.temp;
+    document.getElementById('current-temp').textContent = Math.round(jsObject.main.temp);
     document.getElementById('humidity').textContent = jsObject.main.humidity;
     document.getElementById('wind-speed').textContent = jsObject.wind.speed;
   });
