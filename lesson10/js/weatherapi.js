296ff24bd38a6986b9abb443e7d7103f112222e3
NKGregory <b01194921@byui.edu>
2021-03-11 20:48:58 -0700

    push to check errors

--- lesson10/js/weatherapi.js
+++ lesson10/js/weatherapi.js
@@ -1,42 +1,46 @@
-const apiURL = "https://api.openweathermap.org/data/2.5/weather?id=5604473&units=imperial&appid=36089ae9649ea39369703e6946f36ac3";
-fetch(apiURL)
+const summaryapiURL = "https://api.openweathermap.org/data/2.5/weather?id=5604473&units=imperial&appid=36089ae9649ea39369703e6946f36ac3";
+fetch(summaryapiURL)
   .then((response) => response.json())
   .then((jsObject) => {
-    console.log(jsObject);
+    //console.log(jsObject);
     document.getElementById('currently').textContent = jsObject.weather[0].description;
     document.getElementById('current-temp').textContent = jsObject.main.temp;
     document.getElementById('humidity').textContent = jsObject.main.humidity;
     document.getElementById('wind-speed').textContent = jsObject.wind.speed;
   });
 
-  const foreapiURL = "https://api.openweathermap.org/data/2.5/forecast?id=5604473&units=imperial&appid=36089ae9649ea39369703e6946f36ac3";
-fetch(foreapiURL)
+const forecastapiURL = "https://api.openweathermap.org/data/2.5/forecast?id=5604473&units=imperial&appid=36089ae9649ea39369703e6946f36ac3";
+fetch(forecastapiURL)
   .then((response) => response.json())
   .then((jsObject) => {
-    console.log(jsObject);
-    const substring = '18:00:00'
-    //for (let i = 0; i < jsObject.length; i++ ) {
-     // if(jsObject.list[i].dt_txt.includes("18:00")) {
+    //console.log(jsObject);
+    const ForecastDays = jsObject.list.filter((element)=>element.dt_txt.includes('18:00:00'))
+    //console.log(ForecastDays);
+    const dayTitle = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+    let day = 0
+    for (let i = 0; i < ForecastDays.length; i++ ) {
         let card = document.createElement('section');
-        let day = document.createElement('p')
+        let weekday = document.createElement('p')
         let picture = document.createElement('img')
         let temp = document.createElement('p')
 
         card.setAttribute('class', 'forcastday');
-        const options = {weekday: 'long'}
-        day.textContent =  new Date().toLocaleDateString('en-US', options)
-        //.jsObject.list[4/*i*/].dt_text;
-        const imagesrc = 'https://openweathermap.org/img/w/' + jsObject.list[4/*i*/].weather[0].icon + '.png';  // note the concatenation
-        const desc = jsObject.list[4/*i*/].weather[0].description;  // note how we reference the weather array
+
+        let d = new Date(ForecastDays[i].dt_txt);
+        weekday.textContent = dayTitle[d.getDay()];
+        day++;
+
+        const imagesrc = 'https://openweathermap.org/img/w/' + jsObject.list[i].weather[0].icon + '.png';  // note the concatenation
+        const desc = jsObject.list[i].weather[0].description;  // note how we reference the weather array
         picture.setAttribute('src', imagesrc);  // focus on the setAttribute() method
         picture.setAttribute('alt', desc);
-        temp.innerHTML = Math.round(jsObject.list[4/*i*/].main.temp)+"&deg;"+" F";
 
-        card.appendChild(day);
+        temp.innerHTML = Math.round(jsObject.list[i].main.temp)+"&deg;"+" F";
+
+        card.appendChild(weekday);
         card.appendChild(picture);
         card.appendChild(temp);
 
         document.querySelector('div.forcasttable').appendChild(card);
-      //}
-   // }
+    }
   });
