812b17836d7fbe5142eecec2559d8350502e7440
NKGregory <b01194921@byui.edu>
2021-04-01 22:50:46 -0600

    push for json

--- lesson14/js/directory.js
+++ lesson14/js/directory.js
@@ -1,32 +1,33 @@
-fetch("../data.json")
+fetch('../lesson14/data.json')
   .then(function (response) {
     return response.json();
   })
   .then(function (jsonObject) {
-   console.table(jsonObject);  // temporary checking for valid response and data parsing
-    /*const prophets = jsonObject['prophets'];
+   //console.table(jsonObject);  // temporary checking for valid response and data parsing
+    const businesses = jsonObject['businesses'];
     //prophets.forEach(prophet >{})
 
-    for (let i = 0; i < prophets.length; i++ ) {
+    for (let i = 0; i < businesses.length; i++ ) {
         let card = document.createElement('section');
         let h2 = document.createElement('h2');
-        let birthdate = document.createElement('p')
-        let birthplace = document.createElement('p')
-        let picture = document.createElement('img')
+        let phone = document.createElement('p')
+        let website = document.createElement('p')
+        //let picture = document.createElement('img')
             
-        h2.textContent = prophets[i].name + ' ' + prophets[i].lastname;
-        birthdate.textContent = 'Date of Birth: '+ prophets[i].birthdate;
-        birthplace.textContent = 'Place of Birth: ' + prophets[i].birthplace;
-        picture.setAttribute('src', prophets[i].imageurl);
-        picture.setAttribute('alt', prophets[i].name + ' ' + prophets[i].lastname + '-' + i)
+        h2.textContent = businesses[i].name;
+        //picture.setAttribute('src', businesses[i].imageurl);
+        //picture.setAttribute('alt',)
+        phone.textContent = businesses[i].phone;
+        website.textContent =businesses[i].website;
+
 
 
         card.appendChild(h2);
-        card.appendChild(birthdate);
-        card.appendChild(birthplace);
-        card.appendChild(picture);
+        card.appendChild(phone);
+        card.appendChild(website);
+        //card.appendChild(picture);
 
 
         document.querySelector('div.cards').appendChild(card);
-    }*/
+    }
 });
