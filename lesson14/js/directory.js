715a53ca027d79b80b9735022f9695e3b8633de9
NKGregory <b01194921@byui.edu>
2021-04-03 15:35:16 -0600

    add logo and anchor tags

--- lesson14/js/directory.js
+++ lesson14/js/directory.js
@@ -11,21 +11,23 @@ fetch('../lesson14/data.json')
         let card = document.createElement('section');
         let h2 = document.createElement('h2');
         let phone = document.createElement('p')
-        let website = document.createElement('p')
-        //let picture = document.createElement('img')
+        let website = document.createElement('a')
+        let picture = document.createElement('img')
             
         h2.textContent = businesses[i].name;
-        //picture.setAttribute('src', businesses[i].imageurl);
-        //picture.setAttribute('alt',)
+        picture.setAttribute('src', businesses[i].logo);
+        picture.setAttribute('alt', businesses[i].name + 'logo')
         phone.textContent = businesses[i].phone;
         website.textContent =businesses[i].website;
+        website.setAttribute('href', businesses[i].website)
+        website.setAttribute('target', '_blank')
 
 
 
         card.appendChild(h2);
         card.appendChild(phone);
         card.appendChild(website);
-        //card.appendChild(picture);
+        card.appendChild(picture);
 
 
         document.querySelector('div.cards').appendChild(card);
